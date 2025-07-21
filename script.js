14b2ba186c1ece4844b4fa3f75739c2c589468d5
Leontiadis Konstantinos <113901432+LeonKonst@users.noreply.github.com>
2025-07-21 21:17:11 +0000

    Remove codeswing extension file, create the main function to calculate the ideal gas law formula, fix bug with multiple ids and wrap inputs in divs, add grid display to fieldsets.

--- script.js
+++ script.js
@@ -1,11 +1,42 @@
 let variableToCalculateFieldset = document.getElementById('variableToCalculate');
-let result = document.getElementById('result');
+let result = document.getElementById('resultValue');
+let knownVariablesFieldset = document.getElementById('knownVariables');
+
+
+
+function calculateProperty(selectedInput) {
+    let pressure = parseFloat(document.getElementById('pressureInput').value);
+    let volume = parseFloat(document.getElementById('volumeInput').value);
+    let temperature = parseFloat(document.getElementById('temperatureInput').value);
+    let moles = parseFloat(document.getElementById('molesInput').value);
+
+    switch (selectedInput[0].value) {
+        case 'pressure':
+            return (moles * 0.0821 * temperature) / volume;
+        case 'volume':
+            return (moles * 0.0821 * temperature) / pressure;
+        case 'temperature':
+            return (pressure * volume) / (moles * 0.0821);
+        case 'moles':
+            return (pressure * volume) / (0.0821 * temperature);
+        default:
+            return 'Invalid selection';
+    }
+}
+
 
 variableToCalculateFieldset.addEventListener('change', () => {
-    let variableToCalculate = variableToCalculateFieldset.value;
-    if (variableToCalculate === 'none') {
-        result.textContent = 'Please select a variable to calculate.';
-    } else {
-        result.textContent = `You have selected: ${variableToCalculate}`;
-    }
+    let selectedOptions = variableToCalculateFieldset.querySelectorAll('input[type="radio"]:checked');
+    let selectedInput = document.getElementById(`${selectedOptions[0].value}InputDiv`);
+    
+    knownVariablesFieldset.style.display = 'block';
+    let divs = knownVariablesFieldset.querySelectorAll('div');
+
+    divs.forEach(div => {
+        div.style.display = 'block';
+    });
+
+    selectedInput.style.display = 'none';
+
+    result.textContent = `The ${selectedOptions[0].value} is ${calculateProperty(selectedOptions)}.`;
 })
