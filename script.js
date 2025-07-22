70cc87806c4b70a805ec176da07b884f3b4ec9a7
Leontiadis Konstantinos <113901432+LeonKonst@users.noreply.github.com>
2025-07-22 19:05:39 +0000

    Make the site interactive. Each change in units, values and variable to calculate selection changes the result.
    This change was needed as it makes the site feel more responsive to users interaction

--- script.js
+++ script.js
@@ -1,42 +1,101 @@
-//     let variableToCalculateFieldset = document.getElementById('variableToCalculate');
-// let result = document.getElementById('resultValue');
-// let knownVariablesFieldset = document.getElementById('knownVariables');
+let variableToCalculateFieldset = document.getElementById('variableToCalculate');
+let knownVariablesFieldset = document.getElementById('knownVariables');
+let result = document.getElementById('resultValue');
+let unitSelectors = document.querySelectorAll(".unitSelector");
+let knownInputs = document.querySelectorAll(".knownInput")
+
+
+function addListenerToCalculateResults(elements, change) {
+    elements.forEach(element => {
+        element.addEventListener(change, () => {  
+            displayKnownVariables()
+            result.textContent = displayResults();
+    })}
+)
+}
+
+addListenerToCalculateResults(unitSelectors,"change");
+addListenerToCalculateResults(knownInputs,"input");
+
+variableToCalculateFieldset.addEventListener("change", () => {  
+            displayKnownVariables()
+            result.textContent = displayResults();
+})
+
+
+function displayKnownVariables(){
+    // Fetch the selected unknown variable 
+    let selectedOptions = variableToCalculateFieldset.querySelectorAll('input[type="radio"]:checked');
+    
+    // Display all the input fields in the known variables fieldset
+    let divs = knownVariablesFieldset.querySelectorAll('div');
+    knownVariablesFieldset.style.display = 'block';
+    divs.forEach(div => {
+        div.style.display = 'block';
+    });
+
+    // except the one corresponding to the selected variable
+    let selectedInput = document.getElementById(`${selectedOptions[0].value}InputDiv`);
+    selectedInput.style.display = 'none';
+    return selectedOptions;
+}
+
+function displayResults(){
+    return `The ${displayKnownVariables()[0].value} is ${Math.floor(calculateProperty(displayKnownVariables())*100)/100}.`;
+}
+
+
+function calculateProperty(selectedInput) {
+    // fetch the values from the input fields and parse them to float
+    let pressure = parseFloat(document.getElementById('pressureInput').value);
+    let volume = parseFloat(document.getElementById('volumeInput').value);
+    let temperature = parseFloat(document.getElementById('temperatureInput').value);
+    let moles = parseFloat(document.getElementById('molesInput').value);
+
+    pressure = pressureUnitConvesion(pressure, document.getElementById("pressureUnit").value);
+    volume = volumeUnitConversion(volume, document.getElementById('volumeUnit').value);
+    temperature = temperatureUntiConversion(temperature,document.getElementById("temperatureUnit").value); 
+
+    switch (selectedInput[0].value) {
+        case 'pressure':
+            return (moles * 0.0821 * temperature) / volume;
+        case 'volume':
+            return (moles * 0.0821 * temperature) / pressure;
+        case 'temperature':
+            return (pressure * volume) / (moles * 0.0821);
+        case 'moles':
+            return (pressure * volume) / (0.0821 * temperature);
+    }
+}
+
+function pressureUnitConvesion(pressure,unit){
+    if (unit === 'bar') {
+        return pressure * 1.01325; 
+    } else if (unit === 'psi'){
+        return pressure * 14.6959; 
+    }
+    return pressure; // Assume it's already in atm
+}
+
+function volumeUnitConversion(volume, unit) {
+    if (unit === 'm3') {
+        return volume * 1000; 
+    }
+    return volume; // Assume it's already in liters
+}
+
+function temperatureUntiConversion(temperature, unit){
+    if (unit === 'celsius') {
+        return temperature + 273.15; 
+    }
+    return temperature; // Assume it's already in Kelvin
+}
+
 // let errorPressure = document.getElementById('errorPressure');
 // let errorVolume = document.getElementById('errorVolume');
 // let errorTemperature = document.getElementById('errorTemperature');
 // let errorMoles = document.getElementById('errorMoles');
 // let resultUnit = document.getElementById('resultUnits')
-
-// function calculateProperty(selectedInput) {
-
-//     let pressure = parseFloat(document.getElementById('pressureInput').value);
-//     let volume = parseFloat(document.getElementById('volumeInput').value);
-//     let temperature = parseFloat(document.getElementById('temperatureInput').value);
-//     let moles = parseFloat(document.getElementById('molesInput').value);
-
-//     switch (selectedInput[0].value) {
-//         case 'pressure':
-//             validateVolume();
-//             validateTemperature();
-//             validateMoles();
-//             return (moles * 0.0821 * temperature) / volume;
-//         case 'volume':
-//             validatePressure();
-//             validateTemperature();
-//             validateMoles();
-//             return (moles * 0.0821 * temperature) / pressure;
-//         case 'temperature':
-//             validatePressure();
-//             validateVolume();
-//             validateMoles();
-//             return (pressure * volume) / (moles * 0.0821);
-//         case 'moles':
-//             validatePressure();
-//             validateVolume();
-//             validateTemperature();
-//             return (pressure * volume) / (0.0821 * temperature);
-//     }
-// }
 
 // function validatePressure() {
 //     let pressureInput = document.getElementById('pressureInput');
@@ -94,33 +153,14 @@
 
 
 
-// variableToCalculateFieldset.addEventListener('change', () => {
-//     clearErrors()
-//     let selectedOptions = variableToCalculateFieldset.querySelectorAll('input[type="radio"]:checked');
-//     let selectedInput = document.getElementById(`${selectedOptions[0].value}InputDiv`);
-    
-//     knownVariablesFieldset.style.display = 'block';
-//     let divs = knownVariablesFieldset.querySelectorAll('div');
-
-//     divs.forEach(div => {
-//         div.style.display = 'block';
-//     });
-
-//     selectedInput.style.display = 'none';
-//     if (validateInputs()) {
-//         result.textContent = `The ${selectedOptions[0].value} is ${Math.floor(calculateProperty(selectedOptions)*100)/100} ${resultUnit}.`;
-//     }
-//     else {
-//         result.textContent = 'Please fix the errors.';
-//     }
-// })
+//
 
 // let inputs = knownVariablesFieldset.querySelectorAll('input');
 
 // inputs.forEach(input => {
 
 //     input.addEventListener('input', () => {
-//         let selectedOptions = variableToCalculateFieldset.querySelectorAll('input[type="radio"]:checked'); 
+//         let selectedOptions = variableToCalculateFieldset.querySelectorAll('input[type="radio"]:checked');
 //         result.textContent = `The ${selectedOptions[0].value} is ${Math.floor(calculateProperty(selectedOptions)*100)/100} ${resultUnit}.`;
 //     });
 // });
