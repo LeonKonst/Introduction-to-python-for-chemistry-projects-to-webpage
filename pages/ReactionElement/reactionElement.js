0d20eacb105204374bc301412a98ac62a525f453
Leontiadis Konstantinos <113901432+LeonKonst@users.noreply.github.com>
2025-07-31 08:41:39 +0000

    Create folders for each page and the corresponding js and css files, also fix the directories in anchor elements

---
+++ pages/ReactionElement/reactionElement.js
@@ -0,0 +1,10 @@
+let reactionString = document.getElementById("reactionInput");
+let elementsInReaction = document.getElementById("elementsInReaction");
+
+
+
+reactionString.addEventListener("input", ()=>{
+    console.log("changed");
+    elementsInReaction.innerText = reactionString.value;
+    }
+)
